98da5874bd53155004b0010eea8cb0a28d34b958
AlmirDautovic <aalmir.d@gmail.com>
2023-07-05 19:24:47 +0200

    added loader and functions for loading and finish loading to show/hide loader and content on page

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const quoteText = document.getElementById('quote');
 const author = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     if (!quote.author) {
@@ -20,17 +32,20 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+
     quoteText.textContent = quote.text;
+    complete();
 }
 
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
     } catch (error) {
-
+        console.log(error);
     }
 }
 
